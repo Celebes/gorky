85869131eddb1897cb1933f6373250fb55e34f43
Celebeze <kris1337@gmail.com>
2014-03-19 10:59:18 +0100

    Dodano podstawowy projekt LIBGDX

---
+++ GORKY_LIBGDX/gorky-desktop/src/pl/edu/wat/tim/gorky/Main.java
@@ -0,0 +1,16 @@
+package pl.edu.wat.tim.gorky;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "gorky";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new GorkyGame(), cfg);
+	}
+}
---
+++ GORKY_LIBGDX/gorky-html/src/pl/edu/wat/tim/gorky/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package pl.edu.wat.tim.gorky.client;
+
+import pl.edu.wat.tim.gorky.GorkyGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new GorkyGame();
+	}
+}
