6a81007d7324264e613d7e35cd54ce57cda5c91c
Celebeze <kris1337@gmail.com>
2014-06-05 23:38:17 +0200

    Dodano logo WAT w menu

--- gorky-game/core/src/pl/edu/wat/wcy/tim/gorky/screens/MenuScreen.java
+++ gorky-game/core/src/pl/edu/wat/wcy/tim/gorky/screens/MenuScreen.java
@@ -4,8 +4,13 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MenuScreen extends AbstractGameScreen {
+	
+	SpriteBatch batch;
+	Texture img;
 	
 	public MenuScreen(Game game) {
 		super(game);
@@ -17,6 +22,10 @@ public class MenuScreen extends AbstractGameScreen {
 	public void render(float deltaTime) {
 		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 		
 		if(Gdx.input.isTouched()) {
 			game.setScreen(new GameScreen(game));
@@ -32,10 +41,16 @@ public class MenuScreen extends AbstractGameScreen {
 	public void resize(int width, int height) {}
 
 	@Override
-	public void show() {}
+	public void show() {
+		batch = new SpriteBatch();
+		img = new Texture("images/LOGO_TIM.png");
+	}
 
 	@Override
-	public void hide() {}
+	public void hide() {
+		batch.dispose();
+		img.dispose();
+	}
 
 	@Override
 	public void pause() {}
